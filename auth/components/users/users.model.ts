6e535585d30f7ee035acab32dd7406009a6ab8f1
DATAS CMS <80272701+datasdk@users.noreply.github.com>
2026-05-31 12:51:38 +0200

    Add files via upload

---
+++ auth/components/users/users.model.ts
@@ -0,0 +1,21 @@
+import { Model } from "@tailflow/laravel-orion/lib/model";
+import { UserAttributes } from "../../interfaces/user-attributes.interface";
+import { UserComputed } from "../../interfaces/user-computed.interface";
+import { UserRelations } from "../../interfaces/user-relations.interface";
+
+
+export class Users extends Model<
+  UserAttributes,
+  UserComputed,
+  UserRelations
+> {
+
+
+  public $resource(): string {
+    return 'users';
+  }
+
+  protected override $keyName: string = 'id';
+
+
+}
